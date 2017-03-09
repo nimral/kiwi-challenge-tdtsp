bb625d28403d01bf6eddd7160ab829b39b2cd1c6
Matěj Kocián <matej.kocian@gmail.com>
2017-03-09 18:42:03 +0100

    dp_heuristic: structure for keeping H best pts

--- dp_heuristic.hpp
+++ dp_heuristic.hpp
@@ -33,8 +33,12 @@ typedef std::pair<cid_t, std::bitset<MAX_N>> umkey_t;
 
 struct PartialTour {
     std::vector<cid_t> tour;
-    std::bitset<MAX_N> S;
+    std::bitset<MAX_N> S;  // set of all the nodes visited by this pt
     cost_t cost;
+
+    // index of this pt in the heap in Keeper
+    // (well it feels kind of hackish...)
+    unsigned int heap_idx;
 
     PartialTour() {}
     PartialTour(cid_t k) :
@@ -58,37 +62,163 @@ struct PartialTour {
         return pt;
     }
 };
-    
 
-void keep_H_best(std::vector<PartialTour> & new_partials, unsigned int H)
-{
-    if (new_partials.size() > H) {
-        std::nth_element(
-            new_partials.begin(),
-            new_partials.begin() + H - 1,
-            new_partials.end(),
-            [](const PartialTour & a, const PartialTour & b) {
-                return a.cost < b.cost;
-            }
-        );
-        new_partials.resize(H);
+
+struct HeapElem {
+    cost_t cost;
+
+    // index of corresponding PartialTour in partials
+    unsigned long int idx;
+};
+
+
+struct Keeper {
+    unsigned int H;
+    std::unordered_map<umkey_t, unsigned int> k_S2idx;
+    std::vector<HeapElem> heap;  // max heap ordered by HeapElem.cost
+    std::vector<PartialTour> partials;
+
+    /* Structure for keeping H best partial tours (pt).
+     *  -> Stored in `partials`.
+     *
+     * We don't care about their ordering, but need to be able to replace the
+     * worst one quickly.
+     *  -> There is a max heap with (index to `partials`, pt's cost) pairs
+     *  (actually HeapElems) ordered by cost.
+     *
+     * We are only interested in the best pt for each (k, S) pair
+     *  -> We maintain the mapping from (k, S) to index in `partials` in
+     *  `k_S2idx` so that when a better pt comes, we can place it easily.
+     */
+
+    Keeper(unsigned int H) : H(H)
+    {
+        partials.reserve(H);
+        heap.reserve(H+1);  // 1-indexing for simple child access (i*2, i*2+1)
+        heap.push_back(HeapElem{0, 0});
     }
-}
 
-// step 3
-void keep_H_best(std::vector<PartialTour> & new_partials,
-                 unsigned int H,
-                 std::unordered_map<umkey_t, unsigned int> & k_S2idx)
-{
-    if (new_partials.size() > H) {
-        keep_H_best(new_partials, H);
-        k_S2idx.clear();
-        for (unsigned int i = 0; i < new_partials.size(); i++) {
-            const PartialTour & pt = new_partials[i];
-            k_S2idx[std::make_pair(pt.k(), pt.S)] = i;
+    void heap_print()
+    {
+        for (auto & elem : heap) {
+            std::cout << elem.cost << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    bool heap_is_heap()  // na naa nanana
+    {
+        for (unsigned int i = 2; i < heap.size(); i++) {
+            if (heap[i / 2].cost < heap[i].cost) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool check()
+    {
+        for (unsigned int i = 0; i < partials.size(); i++) {
+            if (heap[partials[i].heap_idx].idx != i) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void heap_update_hidx(unsigned int hidx)
+    {
+        partials[heap[hidx].idx].heap_idx = hidx;
+    }
+
+    // swap elements in heap and don't forget to update pointers to them in the
+    // corresponding pts
+    void heap_swap(unsigned int a, unsigned int b) {
+        auto tmp = heap[a];
+
+        heap[a] = heap[b];
+        heap_update_hidx(a);
+
+        heap[b] = tmp;
+        heap_update_hidx(b);
+    }
+
+    void heap_insert(HeapElem && elem)
+    {
+        unsigned int hidx = heap.size();
+        heap.emplace_back(elem);
+        heap_update_hidx(hidx);
+        while (hidx > 1 && heap[hidx].cost > heap[hidx / 2].cost) {
+            heap_swap(hidx, hidx / 2);
+            hidx = hidx / 2;
         }
     }
-}
+
+    // note this is not your regular decrease-key, as we have max heap
+    void heap_decrease_key(unsigned int hidx, cost_t cost)
+    {
+        heap[hidx].cost = cost;
+        while (hidx * 2 < heap.size()) {
+            int to_swap = -1;
+            if (hidx * 2 + 1 < heap.size()) {
+                if (heap[hidx * 2].cost > heap[hidx].cost) {
+                    if (heap[hidx * 2].cost > heap[hidx * 2 + 1].cost) {
+                        to_swap = hidx * 2;
+                    } else if (heap[hidx * 2 + 1].cost > heap[hidx].cost) {
+                        to_swap = hidx * 2 + 1;
+                    }
+                } else if (heap[hidx * 2 + 1].cost > heap[hidx].cost) {
+                    to_swap = hidx * 2 + 1;
+                }
+            } else if (heap[hidx * 2].cost > heap[hidx].cost) {
+                to_swap = hidx * 2;
+            }
+
+            if (to_swap != -1) {
+                heap_swap(hidx, to_swap);
+                hidx = to_swap;
+            } else {
+                break;
+            }
+        }
+
+    }
+
+    // add one pt to the Keeper if it is good enough
+    void add(const PartialTour && pt) {
+        // is pt with this (k, S) pair stored already?
+        auto key = std::make_pair(pt.k(), pt.S);
+        auto it = k_S2idx.find(key);
+        if (it != k_S2idx.end()) {
+            PartialTour & found = partials[it->second];
+            if (found.cost > pt.cost) {
+                unsigned int hidx = found.heap_idx;
+                found = pt;
+                found.heap_idx = hidx;
+                heap_decrease_key(hidx, pt.cost);
+            }
+        // no stored pt has this (k, S) pair
+        } else {
+            if (partials.size() < H) {
+                partials.emplace_back(pt);
+                heap_insert(HeapElem{pt.cost, partials.size()-1});
+                k_S2idx[key] = partials.size() - 1;
+            } else if (heap[1].cost > pt.cost) {
+                partials[heap[1].idx] = pt;
+                heap_update_hidx(1);
+                k_S2idx[key] = heap[1].idx;
+                heap_decrease_key(1, pt.cost);
+            }
+        }
+    }
+
+    void clear()
+    {
+        k_S2idx.clear();
+        heap.resize(1);
+        partials.clear();
+    }
+};
 
 
 output_t dp_heuristic(int n,
@@ -96,20 +226,12 @@ output_t dp_heuristic(int n,
                       const costs_table_t & costs,
                       unsigned int H)
 {
-    std::vector<PartialTour> partials(1, PartialTour(start));
-    std::vector<PartialTour> new_partials;
-
-    unsigned int max_partials_size = H + H/2;
+    Keeper keeper(H);
+    keeper.add(PartialTour(start));
+    Keeper new_keeper(H);
 
     for (cid_t t = 0; t < n; t++) {
-        // step 2
-        std::unordered_map<umkey_t, unsigned int> k_S2idx;
-        // TODO "If a partial arrival time is less than the cost value of the
-        // worst retained partial tour in current stage so far, store S, k, and
-        // cost values of the new partial tour (?) until the Set and Cost
-        // arrays are filled or all possible partial tours are obtained."
-        cost_t Hth_worst_cost_upper_bound = 0;
-        for (const auto & pt : partials) {
+        for (const auto & pt : keeper.partials) {
             for (cid_t to = 0; to < n; to++) {
                 if (t == n-1 && to != start) {
                     // in the last layer only arcs leading to the beginning of
@@ -118,51 +240,21 @@ output_t dp_heuristic(int n,
                 }
                 cost_t cost = costs[t][pt.k()][to];
                 if (cost >= 0 && !pt.S[to]) {
-
-                    // a little optimization -- skip pts definitely worse than
-                    // than the Hth best pt so far
-                    if (new_partials.size() < H) {
-                        if (Hth_worst_cost_upper_bound < pt.cost + cost) {
-                            Hth_worst_cost_upper_bound = pt.cost + cost;
-                        }
-                    } else if (Hth_worst_cost_upper_bound < pt.cost + cost) {
-                        continue;
-                    }
-
-                    PartialTour new_pt = pt.prolonged(to, cost);
-
-                    // keep only the best partial tour for each (k, S) pair
-                    // (alternative solution to sorting)
-                    auto key = std::make_pair(new_pt.k(), new_pt.S);
-                    auto it = k_S2idx.find(key);
-                    if (it != k_S2idx.end()) {
-                        if (new_partials[it->second].cost >= new_pt.cost) {
-                            new_partials[it->second] = new_pt;
-                        }
-                    } else {
-                        new_partials.emplace_back(new_pt);
-                        k_S2idx[key] = new_partials.size() - 1;
-                    }
-                    if (new_partials.size() >= max_partials_size) {
-                        // step 3
-                        keep_H_best(new_partials, H, k_S2idx);
-                    }
+                    new_keeper.add(pt.prolonged(to, cost));
                 }
             }
         }
-        // step 3
-        keep_H_best(new_partials, H);
-        partials.clear();
-        std::swap(partials, new_partials);
+        keeper.clear();
+        std::swap(keeper, new_keeper);
     }
 
     std::vector<IOArc> output_arcs;
-    if (partials.empty()) {
+    if (keeper.partials.empty()) {
         return output_arcs;
     }
 
     // there can be only one partial tour for S = {0 .. n-1}, k = start
-    const PartialTour & best_pt = partials[0];
+    const PartialTour & best_pt = keeper.partials[0];
 
     output_arcs.reserve(n);
     for (cid_t t = 0; t < n; t++) {
